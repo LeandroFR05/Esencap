ba22380fcb44c1dac6e0f52dbbf8daf4fd9d59d4
LeandroFR05 <leafabian26@gmail.com>
2026-08-06 09:41:41 -0300

    nuevo

--- database/factories/LoteProductoFactory.php
+++ database/factories/LoteProductoFactory.php
@@ -19,7 +19,7 @@ class LoteProductoFactory extends Factory
      */
     public function definition(): array
     {
-        $stockInicial = fake()->random_int(10, 70);
+        $stockInicial = fake()->randomBetween(10, 70);
 
         return [
             'idUsuario' => 1,
