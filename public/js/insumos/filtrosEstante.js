f601137f8f8770624c53f8b3c573479756b1371f
LeandroFR05 <leafabian26@gmail.com>
2026-08-12 10:02:13 -0300

    arreglando filtros

--- public/js/insumos/filtrosEstante.js
+++ public/js/insumos/filtrosEstante.js
@@ -1,11 +1,15 @@
-document.getElementById("insumo").addEventListener("keypress", function(event) {
-    // Permitir enviar con Enter
-    if (event.key === "Enter") {
-        document.getElementById("formFiltros").submit();
-    }
-});
+const inputNombre = document.getElementById("nombre");
+if (inputNombre) {
+    inputNombre.addEventListener("keypress", function(event) {
+        // Permitir enviar con Enter
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("formFiltros").submit();
+        }
+    });
+}
 
-function limpiarFiltrosInsumos() {
+window.limpiarFiltrosInsumos = function() {
     // 1. Limpiar los inputs
     document.getElementById("nombre").value = "";
     document.getElementById("familia").value = "";
@@ -13,4 +17,4 @@ function limpiarFiltrosInsumos() {
 
     // 2. Enviar el formulario vacío (para resetear a todos los registros)
     document.getElementById("formFiltros").submit();
-}
+};
--- public/js/productos/filtrosEstante.js
+++ public/js/productos/filtrosEstante.js
@@ -1,15 +1,19 @@
-document.getElementById("producto").addEventListener("keypress", function(event) {
-    // Permitir enviar con Enter
-    if (event.key === "Enter") {
-        document.getElementById("formFiltros").submit();
-    }
-});
+const inputNombre = document.getElementById("nombre");
+if (inputNombre) {
+    inputNombre.addEventListener("keypress", function(event) {
+        // Permitir enviar con Enter
+        if (event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("formFiltros").submit();
+        }
+    });
+}
 
-function limpiarFiltrosProductos() {
+window.limpiarFiltrosProductos = function() {
     // 1. Limpiar los inputs
     document.getElementById("nombre").value = "";
     document.getElementById("fecha").value = "";
 
     // 2. Enviar el formulario vacío (para resetear a todos los registros)
     document.getElementById("formFiltros").submit();
-}
+};
